87059d751cff941312c1a830c345bcb50f1efbc7
Mark Peng <mrwhy132markmin@gmail.com>
2025-05-02 15:57:57 -0400

    follow eslint rules

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-import { motion, LayoutGroup } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function NavBar() {
     const pathname = usePathname();
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
     <div className="flex flex-col items-center min-h-[calc(100vh-12rem)] justify-center space-y-2 py-4">
       <FadeInSection>
-        <h1 className="text-6xl text-center font-bold">hello, i'm Mark Peng!</h1>
+        <h1 className="text-6xl text-center font-bold">hello, i&apos;m Mark Peng!</h1>
       </FadeInSection>
       <FadeInSection delay={0.2}>
         <h1 className="text-lg text-center">An aspiring software engineer and current student at the University of Toronto studying computer science. </h1>
