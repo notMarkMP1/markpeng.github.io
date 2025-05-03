e64cb0c573688dd28fc4e5792b3b93e9965895a2
Mark Peng <mrwhy132markmin@gmail.com>
2025-05-03 18:49:11 -0400

    project interface kind of done + cannot interact before fade finishes

--- app/components/FadeInSection.tsx
+++ app/components/FadeInSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 
 interface FadeInSectionProps {
@@ -8,11 +8,18 @@ interface FadeInSectionProps {
 }
 
 export default function FadeInSection({ children, delay }: FadeInSectionProps) {
+    const divRef = useRef<HTMLDivElement>(null);
+    
     return (
         <motion.div
+            ref={divRef}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay , ease: "easeInOut" }}
+            transition={{ duration: 0.6, delay, ease: "easeInOut" }}
+            style={{ pointerEvents: "none" }}
+            onAnimationComplete={() => {
+                if (divRef.current) divRef.current.style.pointerEvents = "auto";
+            }}
         >
             {children}
         </motion.div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { useState } from 'react';
+import Image from 'next/image';
 
 interface ProjectProps {
     items: {
@@ -14,6 +16,8 @@ interface ProjectProps {
 
 
 export default function Projects({items}: ProjectProps){
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     const colorDictionary: { [key: string]: string } = {
         "React": "bg-blue-500",
@@ -36,18 +40,34 @@ export default function Projects({items}: ProjectProps){
         "bg-teal-700",
     ];
 
+    const handleMouseMove = (e: React.MouseEvent) => {
+        setMousePosition({
+            x: e.clientX,
+            y: e.clientY
+        });
+    };
+
     return (
-        <>
-        {items.map((item, index) => (
-            <motion.div key={index} className="grid grid-cols-2 gap-4 space-y-4 mb-4 cursor-pointer"
-                whileHover= {{
-                    scale: 1.05,
-                    boxShadow: "0px 5px 0px rgba(0, 0, 0, 0.1)"
-                }}
-                whileTap= {{
-                    scale: 0.98}}
-                transition={{ type: "spring", stiffness: 300, damping: 20 }}
-            >
+        <div className="relative" onMouseMove={handleMouseMove}>
+            {items.map((item, index) => (
+                <motion.div 
+                    key={index} 
+                    className="grid grid-cols-2 gap-4 space-y-4 mb-4 cursor-pointer px-1 relative"
+                    whileHover= {{
+                        scale: 1.05,
+                        boxShadow: "0px 5px 0px rgba(0, 0, 0, 0.1)"
+                    }}
+                    whileTap= {{
+                        scale: 0.98
+                    }}
+                    transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                    onClick={() => {
+                        window.open(item.link, "_blank");
+                        setHoveredIndex(null);
+                    }}
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                >
                     <div className="col-span-1 flex flex-col space-y-0.5">
                         <h3 className="text-md font-semibold">{item.title}</h3>
                         <p className="text-sm text-zinc-700">{item.description}</p>
@@ -57,8 +77,32 @@ export default function Projects({items}: ProjectProps){
                             <span key={techIndex} className={`inline-block text-xs rounded-full px-2 py-1 mr-0.5 text-white ${colorDictionary[tech] || colorClasses[tech.length % colorClasses.length]} whitespace-nowrap`}>{tech}</span>
                         ))}
                     </div>
-            </motion.div>        
-        ))}
-        </>
+                </motion.div>        
+            ))}
+            {hoveredIndex !== null && items[hoveredIndex].imagePath && (
+                <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0 }}
+                    className="fixed z-50 rounded-lg overflow-hidden"
+                    style={{ 
+                        left: `${mousePosition.x - 150}px`,
+                        top: `${mousePosition.y - 220}px`,
+                        width: '300px',
+                        height: '200px'
+                    }}
+                >
+                    <Image 
+                        src={items[hoveredIndex].imagePath} 
+                        alt={items[hoveredIndex].title} 
+                        data-loaded = 'false'
+                        onLoad = {(e) => {e.currentTarget.setAttribute('data-loaded', 'true')}}
+                        width={300}
+                        height={200}
+                        className="data-[loaded=false]:animate-pulse data-[loaded=false]:bg-zinc-100/50 w-full h-full object-cover"
+                    />
+                </motion.div>
+            )}
+        </div>
     );
 }
